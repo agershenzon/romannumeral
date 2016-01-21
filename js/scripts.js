4fa44036a368e32daa295802249d336fd5019597
Aaron Gershenzon and Alex Larson <alex.m.larson@gmail.com>
2016-01-21 13:20:16 -0800

    added html functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,29 @@
-s
 
+var splitword = function (word) {
+var chop = word.split("");
+return chop;
+}
 
-$(document).ready(function() {
-
+var findvar = function (word) {
+var letters = splitword(word)
+var newarray = []
+letters.forEach(function(i) {
+  if(i.match(/[aeiou]/)){
+    i = "-"
+    newarray.push(i)
+  }else {
+    newarray.push(i)
+    }
   });
-});
+var results = newarray.join("");
+return results;
+};
+
+$(function () {
+  $("button").click(function(){
+    var results = $("#hide").val();
+    var runFunction = findvar(results);
+    $("p#results").text(runFunction)
+  })
+
+})
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
-describe("replace", function(){
-  it("should find a string and plit into array", function(){
-    expect(countBy("ant")).to.equal(["a","n","t"])
+describe("splitword", function(){
+  it("should find a string and split into array", function(){
+    expect(splitword("ant")).to.equal["a","n","t"]
   });
 });
+
+  describe("findvar", function(){
+  it("should find a vowel and replace each vowel with a '-'", function(){
+    expect(findvar("ant")).to.equal["-","n","t"]
+  });
+  });
