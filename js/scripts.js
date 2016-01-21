7a8426beb71ec22fc578c384785697f417904f90
Aaron Gershenzon and Alex Larson <alex.m.larson@gmail.com>
2016-01-21 11:09:08 -0800

    add first rule to specs.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,7 @@
-var countBy = function (incr,max) {
-  var text = "";
-  for (var i = incr; i < max; i+=incr) {
-    text = text + (i + ", ")
-  }
-
-  return text;
-}
+s
 
 
 $(document).ready(function() {
-  $("button").click(function(){
-
-    var input1 = parseInt($("#increment").val());
-    var input2 = parseInt($("#max").val());
-    var funct = countBy(input1, input2);
-    $("#results").text("");
-    $("#results").append(funct);
 
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
-describe("countBy", function(){
-  it("should take an input and increment by 5 while starting at 0", function(){
-    expect(countBy(5,25)).to.equal(25)
+describe("replace", function(){
+  it("should find the vowels and replace with -", function(){
+    expect(countBy("ant")).to.equal("-nt")
   });
 });
