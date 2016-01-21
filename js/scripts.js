3004a8a62b5b82b41e51a2d72d2ee96f252a4d9e
Aaron Gershenzon and Alex Larson <aaron@urbankayaks.com>
2016-01-21 10:56:30 -0800

    finished functionality on couting project

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
+var countBy = function (incr,max) {
+  var text = "";
+  for (var i = incr; i < max; i+=incr) {
+    text = text + (i + ", ")
+  }
+
+  return text;
+}
+
+
 $(document).ready(function() {
-alert("hellO?")
+  $("button").click(function(){
 
+    var input1 = parseInt($("#increment").val());
+    var input2 = parseInt($("#max").val());
+    var funct = countBy(input1, input2);
+    $("#results").text("");
+    $("#results").append(funct);
 
-})
+  });
+});
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("countBy", function(){
+  it("should take an input and increment by 5 while starting at 0", function(){
+    expect(countBy(5,25)).to.equal(25)
+  });
+});
